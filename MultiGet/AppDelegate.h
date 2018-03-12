2ab082b25c2c5014c9618b63cacfd327713be201
Clive <clive@wcapps.com>
2018-03-12 15:25:08 +0000

    Initial Commit

---
+++ MultiGet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiGet
+//
+//  Created by Clive on 12/03/2018.
+//  Copyright © 2018 World Class Apps Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiGet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiGet
+//
+//  Created by Clive on 12/03/2018.
+//  Copyright © 2018 World Class Apps Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
